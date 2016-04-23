0dbf4f42f2c5eada3fab58cda529e7c871fa1eab
Tom Purucker <purucker.tom@gmail.com>
2016-04-23 00:07:17 -0400

    requests

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
 from beekeeper import API
+import requests
+
+resp = requests.get('http://qed.epa.gov/rest/ubertool/earthworm/').json()
 
 wiki = API.from_hive_file('qed.epa.gov/api/#!/agdrift/get_rest_ubertool_agdrift.json')
 
-uber = API.from_domain('http://qed.epa.gov/api/#!/agdrift/get_rest_ubertool_agdrift', require_https=False)
+uber = API.from_domain('qed.epa.gov/rest/ubertool/earthworm/', require_https=False)
