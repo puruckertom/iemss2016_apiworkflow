11f3a79099a5a9ffb7dcdec8a9dc68e57faece91
Tom Purucker <purucker.tom@gmail.com>
2016-04-23 23:28:09 -0400

    status

--- api.py
+++ api.py
@@ -1,8 +1,18 @@
-from beekeeper import API
+#from beekeeper import API
 import requests
 
-resp = requests.get('http://qed.epa.gov/rest/ubertool/earthworm/').json()
+#add ipynb for python3
 
-wiki = API.from_hive_file('qed.epa.gov/api/#!/agdrift/get_rest_ubertool_agdrift.json')
+url = 'http://qed.epa.gov/rest/ubertool/earthworm/'
 
-uber = API.from_domain('qed.epa.gov/rest/ubertool/earthworm/', require_https=False)
+resp = requests.get(url)
+
+resp.status_code
+
+resp.headers['content-type']
+
+resp.encoding
+
+resp.text
+
+resp.json()
